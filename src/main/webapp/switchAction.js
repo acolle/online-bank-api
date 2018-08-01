8bd7ef9c772fa353165017c172de4441bfee42e7
Anthony Colle <anthonycolle84@gmail.com>
2018-08-01 17:49:05 +0100

    Profile view added

---
+++ src/main/webapp/switchAction.js
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Display Existing User or New User Views
+ function displayExistingUser() {
+     document.getElementById("existingUser").style.display = "block";
+     document.getElementById("newUser").style.display = "none";
+ }
+
+ function displayNewUser() {
+   document.getElementById("existingUser").style.display = "none";
+   document.getElementById("newUser").style.display = "block";
+ }
+
+// Display Selected Action
+function displayTransfer() {
+    document.getElementById("transfer-form").style.display = "block";
+    document.getElementById("transaction-history").style.display = "none";
+    document.getElementById("new-account-form").style.display = "none";
+}
+
+function displayTransactionHistory() {
+  document.getElementById("transfer-form").style.display = "none";
+  document.getElementById("transaction-history").style.display = "block";
+  document.getElementById("new-account-form").style.display = "none";
+}
+
+function displayNewAccount() {
+  document.getElementById("transfer-form").style.display = "none";
+  document.getElementById("transaction-history").style.display = "none";
+  document.getElementById("new-account-form").style.display = "block";
+}
+
+function displayNewAccount2() {
+  document.getElementById("new-account-form-2").style.display = "block";
+}
