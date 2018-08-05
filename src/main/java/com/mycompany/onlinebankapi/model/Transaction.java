9117313fb9e8647c2f8d6d0f62b360a91f98e62e
Stephen <stiofan.o.heimhin@gmail.com>
2018-08-05 12:34:49 +0100

    Fixed Transaction being a MappedSuperclass instead of Entity.

--- src/main/java/com/mycompany/onlinebankapi/model/Transaction.java
+++ src/main/java/com/mycompany/onlinebankapi/model/Transaction.java
@@ -7,17 +7,17 @@ package com.mycompany.onlinebankapi.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToOne;
 
 /**
  *
  * @author anthonycolle
  */
-@MappedSuperclass
+@Entity
 public class Transaction implements Serializable {
 
     public static final String
