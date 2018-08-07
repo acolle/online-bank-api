a74beaec4e67d33b20ca3696c43521352ea9fafc
Stephen <stiofan.o.heimhin@gmail.com>
2018-08-07 19:53:33 +0100

    Modified some method headers.

--- src/main/java/com/mycompany/onlinebankapi/resources/AccountResource.java
+++ src/main/java/com/mycompany/onlinebankapi/resources/AccountResource.java
@@ -8,11 +8,15 @@ package com.mycompany.onlinebankapi.resources;
 import com.mycompany.onlinebankapi.model.Account;
 import com.mycompany.onlinebankapi.service.AccountService;
 import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -29,26 +33,39 @@ public class AccountResource {
 
     // Display a list of all accounts for current user
     @GET
-    @Path("/all")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Account> getAccounts() {
-        return accountService.retrieveAccounts();
+    public List<Account> getAccounts(@CookieParam("mainaccount") Cookie cookie) {
+        //TODO : Get only accounts associated with signed in user
+		return accountService.retrieveAccounts();
     }
     
     // Create a new account
     @GET
     @Path("/new")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response newAccount(@Context UriInfo info) {
-       return null; 
+    public Response newAccountForm() {
+		//Return form, submit points to POST /accounts with new account
+		return null; 
+    }
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response newAccount(@CookieParam("mainaccount") Cookie cookie, Account account) {
+       //Return form, submit points to /accounts POST
+		return null; 
     }
 
     // Display a specific account (get balance)
     @GET
-    @Path("/accounts/{accountId}")
+    @Path("/{accountId}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Account getAccount(@PathParam("accountId") int id) {
+    public Account getAccount(@CookieParam("mainaccount") Cookie cookie, @PathParam("accountId") int id) {
         //return accountService.getAccount(id);
+		/*
+		If account with 'id' does not belong to customer return error message
+		with accounts associated with customer
+		*/
         return null;
     }
     
