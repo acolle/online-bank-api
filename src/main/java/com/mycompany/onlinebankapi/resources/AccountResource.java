d33b9871f5c654146665bb192868959685c28260
Stephen <stiofan.o.heimhin@gmail.com>
2018-08-18 12:41:27 +0100

    Finished AccountResource. Needs testing.

--- src/main/java/com/mycompany/onlinebankapi/resources/AccountResource.java
+++ src/main/java/com/mycompany/onlinebankapi/resources/AccountResource.java
@@ -6,7 +6,11 @@
 package com.mycompany.onlinebankapi.resources;
 
 import com.mycompany.onlinebankapi.model.Account;
+import com.mycompany.onlinebankapi.model.ErrorMessage;
 import com.mycompany.onlinebankapi.service.AccountService;
+import com.mycompany.onlinebankapi.service.CustomerService;
+import com.mycompany.onlinebankapi.service.Hasher;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.CookieParam;
@@ -15,11 +19,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
 /**
  *
@@ -29,22 +32,47 @@ import javax.ws.rs.core.UriInfo;
 @Path("/accounts")
 public class AccountResource {
     
+	private static final int ADMIN_ACCOUNT = 1;
+	
     AccountService accountService = new AccountService();
 
     // Display a list of all accounts for current user
     @GET
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Account> getAccounts(@CookieParam("mainaccount") Cookie cookie) {
+    public Response getAccounts(@CookieParam("mainaccount") Cookie cookie) {
         //TODO : Get only accounts associated with signed in user
-		return accountService.retrieveAccounts();
+		if(cookie == null)
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(new ErrorMessage("Not signed in, cannot get list of accounts."))
+					.cookie(new NewCookie(cookie, null, 0, false))
+					.build();
+		if(Hasher.decryptId(cookie.getValue()) <= 0)
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(new ErrorMessage("Invalid account cookie removed. Log in and try again."))
+					.cookie(new NewCookie(cookie, null, 0, false))
+					.build();
+		int uid = Hasher.decryptId(cookie.getValue());
+		List<Account> ret = new ArrayList<>();
+		for(Account a : accountService.retrieveAccounts())
+			if(a.getCustomer().getId() == uid)
+				ret.add(a);
+		return Response.ok(ret).build();
     }
 	
 	//Gets all accounts from all users (admin only)
 	@GET
 	@Path("/all")
 	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-	public List<Account> getAllAccount(@CookieParam("mainaccount") Cookie cookie) {
-		return null; //TODO
+	public Response getAllAccount(@CookieParam("mainaccount") Cookie cookie) {
+		if(cookie == null || Hasher.decryptId(cookie.getValue()) != ADMIN_ACCOUNT)
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(new ErrorMessage("Cannot view all accounts unless on an admin profile."))
+					.cookie(new NewCookie(cookie, null, 0, false))
+					.build();
+		return Response.ok(accountService.allEntries()).build();
 	}
     
     // Create a new account
@@ -60,21 +88,54 @@ public class AccountResource {
 	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Response newAccount(@CookieParam("mainaccount") Cookie cookie, Account account) {
-       //Return form, submit points to /accounts POST
-		return null; 
+		if(cookie == null)
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(new ErrorMessage("Not signed in, cannot create new account."))
+					.cookie(new NewCookie(cookie, null, 0, false))
+					.build();
+		int uid = Hasher.decryptId(cookie.getValue());
+		if(uid <= 0)
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(new ErrorMessage("Invalid account cookie removed. Log in and try again."))
+					.cookie(new NewCookie(cookie, null, 0, false))
+					.build();
+		//Make sure the customer association is set properly for the signed in user
+		account.setCustomer(CustomerService.retrieveCustomer(uid));
+		return Response.ok(accountService.createAccount(account)).build();
     }
 
     // Display a specific account (get balance)
     @GET
     @Path("/{accountId}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Account getAccount(@CookieParam("mainaccount") Cookie cookie, @PathParam("accountId") int id) {
+    public Response getAccount(@CookieParam("mainaccount") Cookie cookie, @PathParam("accountId") int id) {
         //return accountService.getAccount(id);
 		/*
 		If account with 'id' does not belong to customer return error message
 		with accounts associated with customer
 		*/
-        return null;
-    }
+		if(cookie == null)
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(new ErrorMessage("Not signed in, cannot get account."))
+					.cookie(new NewCookie(cookie, null, 0, false))
+					.build();
+		int cid = Hasher.decryptId(cookie.getValue());
+		if(cid <= 0)
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(new ErrorMessage("Invalid account cookie removed. Log in and try again."))
+					.cookie(new NewCookie(cookie, null, 0, false))
+					.build();
+		Account a = accountService.retrieveAccount(id);
+		if(a.getCustomer().getId()!=cid)
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(new ErrorMessage("Cannot access other users accounts."))
+					.build();
+		return Response.ok(a).build();
+	}
     
 }
