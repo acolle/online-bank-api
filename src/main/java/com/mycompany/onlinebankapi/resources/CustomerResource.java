48ccc6004cb8ea59d41bee3825d5c6c11c494076
Stephen <stiofan.o.heimhin@gmail.com>
2018-08-18 13:11:58 +0100

    Gutted method to make way for new version. Cleared unused imports.

--- src/main/java/com/mycompany/onlinebankapi/resources/CustomerResource.java
+++ src/main/java/com/mycompany/onlinebankapi/resources/CustomerResource.java
@@ -10,7 +10,6 @@ import com.mycompany.onlinebankapi.model.ErrorMessage;
 import com.mycompany.onlinebankapi.model.LoginCredentials;
 import com.mycompany.onlinebankapi.service.CustomerService;
 import com.mycompany.onlinebankapi.service.Hasher;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
@@ -20,13 +19,11 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.UriInfo;
 
 /**
  *
@@ -135,57 +132,16 @@ public class CustomerResource {
     @GET
     @Path("/new")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response newUser(@Context UriInfo info) {
-
-        try {
-
-//            // Check values from UriInfo
-//            System.out.println("absolutePath: " + info.getAbsolutePath());
-//            System.out.println("baseUri: " + info.getBaseUri());
-//            System.out.println("matchedResource: " + info.getMatchedResources());
-//            System.out.println("matchedUri: " + info.getMatchedURIs());
-//            System.out.println("path: " + info.getPath());
-//            System.out.println("pathParameters: " + info.getPathParameters());
-//            System.out.println("pathSegments: " + info.getPathSegments());
-//            System.out.println("queryParameters: " + info.getQueryParameters());
-//            System.out.println("requestUri: " + info.getRequestUri());
-            // Get the user inputs when creating a new profile
-            String firstname = info.getQueryParameters().getFirst("firstname");
-            String lastname = info.getQueryParameters().getFirst("lastname");
-            String address = info.getQueryParameters().getFirst("address");
-            String email = info.getQueryParameters().getFirst("email");
-            String password = info.getQueryParameters().getFirst("password");
-            String passwordConfirm = info.getQueryParameters().getFirst("password2");
-
-            // Check if all inputs are correct
-            System.out.println("Firstname: " + firstname);
-            System.out.println("Lastname: " + lastname);
-            System.out.println("Address: " + address);
-            System.out.println("Email: " + email);
-            System.out.println("Password: " + password);
-            System.out.println("Password Confirmation: " + passwordConfirm);
-
-            if (firstname.isEmpty() || lastname.isEmpty()
-                    || address.isEmpty() || email.isEmpty()
-                    || password.isEmpty() || passwordConfirm.isEmpty()) {
-
-                // HOW TO RETURN FIELDS THAT HAVE MISSING VALUES?
-                String output = "Please fill in all the fields";
-                return Response.status(200).entity(output).build();
-
-            } else if (!password.equals(passwordConfirm)) {
-                String output = "Sorry the passwords do no match";
-                return Response.status(200).entity(output).build();
-
-            } else {
-                //userService.createCustomer(1, firstname, lastname, address, email);
-                String output = "Welcome " + firstname + "!\nGet started by opening a new account";
-                return Response.status(200).entity(output).build();
-            }
-        } catch (NullPointerException e) {
-            String output = "Error with the parameters passed";
-            return Response.status(200).entity(output).build();
-        }
+    public Response newUser(@CookieParam("mainaccount") Cookie cookie) {
+		if(cookie != null) {
+			//User is already signed in. React accordingly
+			//Tell user to log out first if they want to create a new customer account
+		}
+		//User not signed in, show html form for creating a new customer
+		//and on submit point to POST
+		
+		//for now (placeholder)
+		return Response.serverError().entity("Currently not implemented.").build();
     }
 
     // Display certain details about a specific user (admin)
